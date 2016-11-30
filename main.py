317da197ad5eb57198565aef0dc5c6064e612d9e
Dario Clavijo <daedalus2027@gmail.com>
2016-11-30 19:30:09 -0200

    update

--- main.py
+++ main.py
@@ -10,13 +10,37 @@ import dns_resolver
 import fileinput
 
 nodes = []
-
+limit = 5000
 
 def broadcast(tx):
-	nodes = dns_resolver.nslookup('bitseed.xf2.org')
+	#nodes = dns_resolver.nslookup('bitseed.xf2.org')
+
+	nodes = []
+	c = 0
+
+	fp = open('/tmp/nodes.txt')
+
+	for node in fp:
+		node = node.replace('\n','').replace('\r','')
+		
+		if c <= limit:
+			if (node.find("[") > 0 and node.find("]") > 0):
+				node = node.replace("]:","] ").split(" ")
+			else:
+				node = node.split(':')
+			
+			nodes.append(node)
+			c += 1
+		else:
+			break
+
+	#nodes = nodes[:100]
+	
+	random.shuffle(nodes)
+
 	for node in nodes:
-        	print "Sending TX: %s to node: %s" % (tx,node)
-		relay.relayTx(tx,(node,8333))
+        	print "Sending TX: %s to node: %s" % (tx,node[0])
+		relay.relayTx(tx,(node[0],int(node[1])))
 
 def main():
 	if len(sys.argv)>1:
